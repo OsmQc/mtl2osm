fd0e522c2d58db6539c2083fd3db3b83ba62cc3d
Guillaume Pratte <guillaume@guillaumepratte.net>
2014-11-03 19:46:52 -0500

    Mise a jour traducteur postes de police

--- donnees/postes-de-quartier-de-la-police/traducteur-postes-police.py
+++ donnees/postes-de-quartier-de-la-police/traducteur-postes-police.py
@@ -3,6 +3,9 @@
 Ce fichier est publié sous licence CC0:
 http://creativecommons.org/publicdomain/zero/1.0/
 
+Documentation d'importation pour ce jeu de données:
+http://wiki.openstreetmap.org/wiki/Montr%C3%A9al/Imports/Postes_de_police
+
 Format en entrée:
  Fichier KML contenant les balises suivantes:
   Name: nom du poste
@@ -11,10 +14,10 @@ Format en entrée:
 Format en sortie:
  Fichier OSM avec les balises suivantes:
   amenity=police
+  operator=Service de police de la ville de Montréal
   name
   addr:housenumber
   addr:street
-  addr:city
 
 """
 
@@ -22,6 +25,8 @@ ABBREVIATIONS = {
     'boul': 'boulevard',
     'aven': 'avenue',
     'stre': 'rue',
+    'St-': 'Saint-',
+    'Ste-': 'Sainte-',
 } 
 
 def filterTags(attrs):
@@ -30,6 +35,7 @@ def filterTags(attrs):
 
     tags = {
         'amenity': 'police',
+        'operator': u'Service de police de la ville de Montréal'
     }
 
     if 'Name' in attrs:
@@ -37,11 +43,13 @@ def filterTags(attrs):
 
     if 'description' in attrs:
         adresse, ville = attrs['description'].split('<br/>')[1:3]
-        ville = ville.split('<')[0]
+        #ville = ville.split('<')[0]
         tags['addr:housenumber'] = adresse.split()[0]
         tags['addr:street'] = adresse.split(' ', 1)[1]
         for abbr in ABBREVIATIONS:
             tags['addr:street'] = tags['addr:street'].replace(abbr, ABBREVIATIONS[abbr])
-        tags['addr:city'] = ville
+        tags['addr:street'] = tags['addr:street'][0].upper() + tags['addr:street'][1:]
+        if tags['addr:street'] == u'Avenue 15e':
+            tags['addr:street'] = u'15e Avenue'
 
     return tags
