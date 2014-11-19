25b7c1ad639f9dcb0cec35333161914a34cd9933
Mathieu Leduc-Hamel <mlhamel@ajah.ca>
2014-11-18 19:36:12 -0500

    fixed path to the extract script

--- extract.py
+++ extract.py
@@ -1,13 +1,13 @@
 import fiona
 from shapely.geometry import shape, mapping
 
-grid = fiona.open('data/hot_osmtm_619.json', 'r')
+grid = fiona.open('donnees/quadrillage_example.json', 'r')
 print grid
 
 from rtree import index
 idx = index.Index()
 
-gns = fiona.open('data/gns_sierra_leone_2014_06_14_sl_test_ppl_osm.geojson', 'r')
+gns = fiona.open('donnees/jardins-communautaires/osm/jardins-communautaires.osm', 'r')
 
 for pos, point in enumerate(gns):
     geom = shape(point['geometry'])
@@ -20,9 +20,10 @@ for task in grid:
 
     schema = gns.schema.copy()
     p = task['properties']
-    filename = 'export/gns_%s_%s_%s.shp' % (p['zoom'], p['x'], p['y'])
+    filename = 'export/gns_%s_%s_%s.osm' % (p['zoom'], p['x'], p['y'])
 
     overlapping = [gns[pos] for pos in idx.intersection(polygon.bounds)]
+
     with fiona.collection(filename, 'w', 'ESRI Shapefile', schema=gns.schema, crs=gns.crs) as output:
         for point in overlapping:
             geom = shape(point['geometry'])
