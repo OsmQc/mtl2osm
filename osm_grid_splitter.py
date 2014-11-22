f2f5c32bc89c6f2e11ec85ea770555cdac95ee73
Guillaume Pratte <guillaume@guillaumepratte.net>
2014-11-22 16:40:01 -0500

    Add documentation

--- osm_grid_splitter.py
+++ osm_grid_splitter.py
@@ -1,3 +1,23 @@
+"""
+A program to split a .osm file based on a grid provided in GeoJSON format.
+
+Copyright 2014 Guillaume Pratte <guillaume@guillaumepratte.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 import sys
 import json
 
@@ -14,6 +34,8 @@ def get_xml_nodes(osm_filename):
         yield element
 
 if __name__ == '__main__':
+    # TODO: use optparse
+    # TODO: read the output filename from the arguments
     osm_filename = sys.argv[1]
     json_filename = sys.argv[2]
 
@@ -27,6 +49,7 @@ if __name__ == '__main__':
     # A dictionary to "classify" each OSM points into each grid
     result = {}
 
+    # Intersect the points from the .osm file with the JSON grid
     for xmlnode in get_xml_nodes(osm_filename):
         lon = float(xmlnode.get('lon'))
         lat = float(xmlnode.get('lat'))
@@ -35,9 +58,11 @@ if __name__ == '__main__':
             if square_shape.intersects(point):
                 result.setdefault(json_feature['id'], []).append(xmlnode)
 
+    # Write the set of .osm files, one per grid element
     for grid_id, xmlnodes in result.iteritems():
         with open('out_%s.osm' % grid_id, 'wb') as output:
             output.write('<?xml version="1.0"?>\n<osm version="0.6" upload="false" generator="osm_grid_splitter">\n')
+            # We need to reinitialize the id for each .osm file
             osmid = -1
             for xmlnode in xmlnodes:
                 xmlnode.set('id', str(osmid))
